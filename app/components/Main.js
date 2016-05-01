5740b6780c53a4ffdcccb65cdbd9aea7df7b1e25
Zulfiya <yusupova.zulfiya@gmail.com>
2016-05-01 19:48:12 +0600

    change bundle to main

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,13 +4,11 @@ var ReactDom = require('react-dom');
 var Main = React.createClass({
 	render: function(){
 		return(
-			<div>
-			Hello world
-			<\/div>
+			<div>Hello world</div>
 
-			);
+			)
 
 	}
-});
+})
 
 ReactDom.render(<Main />, document.getElementById('app'));
